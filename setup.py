177c4e88666d9ac55d3e1095f234fd20faf584e4
Ryan Soklaski <ry26099@mit.edu>
2021-04-05 10:44:45 -0400

    Create project structure

---
+++ setup.py
@@ -0,0 +1,47 @@
+import versioneer
+from setuptools import find_packages, setup
+
+DISTNAME = "hydra_utils"
+LICENSE = "MIT"
+AUTHOR = "Justin Goodwin, Ryan Soklaski"
+AUTHOR_EMAIL = "ryan.soklaski@ll.mit.edu"
+URL = "https://github.com/mitll-SAFERai/hydra_utils"
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Science/Research",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Topic :: Scientific/Engineering",
+]
+KEYWORDS = "machine learning research configuration scalable reproducible",
+INSTALL_REQUIRES = ["hydra-core >= 1.1.0dev5"]
+TESTS_REQUIRE = ["pytest >= 3.8", "hypothesis >= 5.32.0"]
+
+DESCRIPTION = "Utilities for making hydra scale to ML workflows"
+LONG_DESCRIPTION = """
+TBD
+"""
+
+
+setup(
+    name=DISTNAME,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license=LICENSE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    url=URL,
+    download_url="https://github.com/mitll-SAFERai/hydra_utils/tarball/" + versioneer.get_version(),
+    python_requires=">=3.7",
+    packages=find_packages(where="src", exclude=["tests", "tests.*"]),
+    package_dir={"": "src"},
+)
