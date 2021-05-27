2fb9d539c8c6ddfefcc65f6c04b9eaa53000600a
Ryan Soklaski <ry26099@mit.edu>
2021-05-27 17:17:59 -0400

    add save/load yaml aliases to top-level namespace

--- src/hydra_zen/__init__.py
+++ src/hydra_zen/__init__.py
@@ -1,7 +1,13 @@
 # Copyright (c) 2021 Massachusetts Institute of Technology
 # SPDX-License-Identifier: MIT
 
-from ._hydra_overloads import MISSING, instantiate, to_yaml
+from ._hydra_overloads import (
+    MISSING,
+    instantiate,
+    load_from_yaml,
+    save_as_yaml,
+    to_yaml,
+)
 from ._version import get_versions
 from .structured_configs import builds, hydrated_dataclass, just, mutable_value
 
