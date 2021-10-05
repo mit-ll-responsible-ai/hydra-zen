94d69f1459e63b8cda2b720b7650b538e0685f38
Ryan Soklaski <ry26099@mit.edu>
2021-10-05 16:35:38 -0400

    improve docs for pydantic and beartype; add optional dependencies

--- src/hydra_zen/experimental/third_party/beartype.py
+++ src/hydra_zen/experimental/third_party/beartype.py
@@ -11,6 +11,73 @@ __all__ = ["validates_with_beartype"]
 
 
 def validates_with_beartype(obj: _T) -> _T:
+    """Decorates a function or the init-method of an object with beartype [1]_.
+
+    This can be used with
+
+    Parameters
+    ----------
+    obj : Callable
+
+    Returns
+    -------
+    obj_w_validation : Callable
+        A wrapped function, or a class whose init-method has been
+        wrapped in-place
+
+    Notes
+    -----
+    ``beartype`` must be installed [2]_ as a separate dependency to leverage this validator.
+
+    hydra-zen adds a sequence-coercion step that is not performed by beartype.
+    This is implemented by `hydra_zen.experimental.coerce.coerce_sequences`. This
+    coercion is necessary as Hydra can only read sequence data in as a list. I.e.
+    without this coercion step, all non-list annotated sequence fields populated by
+    Hydra would get roared at (raised) by beartype.
+
+    See Also
+    --------
+    hydra_zen.experimental.coerce.coerce_sequences
+
+    References
+    ----------
+    .. [1] https://github.com/beartype/beartype
+    .. [2] https://github.com/beartype/beartype#install
+
+    Examples
+    --------
+    >>> from hydra_zen.experimental.third_party.beartype import validates_with_beartype
+    >>> from beartype.cave import ScalarTypes
+    >>> def f(x: ScalarTypes): return x  # a scalar is any real-valued number
+    >>> val_f = validates_with_beartype(f)
+    >>> f([1, 2])  # doesn't catch bad input
+    [1, 2]
+    >>> val_f([1, 2])
+    BeartypeCallHintPepParamException: @beartyped f() parameter x=[1, 2] violates type hint [...]
+
+    >>> class A:
+    ...     def __init__(self, x: ScalarTypes): ...
+    >>> validates_with_beartype(A)  # wrapping occurs in-place
+    __main__.A
+    >>> A([1, 2])
+    BeartypeCallHintPepParamException: @beartyped A.__init__() parameter x=[1, 2] violates type hint [...]
+
+    This is designed to be used with the ``zen_wrappers`` feature of `builds`.
+
+    >>> from hydra_zen import builds, instantiate
+    >>> # instantiations of `conf` will be validated by beartype
+    >>> conf = builds(f, populate_full_signature=True, zen_wrappers=validates_with_beartype)
+    >>> instantiate(conf, x=10)  # 10 is a scalar: ok!
+    10
+    >>> instantiate(conf, x=[1, 2])
+    BeartypeCallHintPepParamException: @beartyped f() parameter x=[1, 2] violates type hint [...]
+
+    Note that sequence-coercion is enabled to ensure smooth compatibility with Hydra.
+
+    >>> def g(x: tuple): return x  # note the annotation
+    >>> validates_with_beartype(g)([1, 2, 3])  # input: list, output: tuple
+    (1, 2, 3)
+    """
     if inspect.isclass(obj) and hasattr(type, "__init__"):
         obj.__init__ = bt.beartype(obj.__init__)
         target = obj
--- src/hydra_zen/experimental/third_party/pydantic.py
+++ src/hydra_zen/experimental/third_party/pydantic.py
@@ -14,7 +14,7 @@ _default_validator = _pyd.validate_arguments(config={"arbitrary_types_allowed": 
 def validates_with_pydantic(
     obj: _T, validator: Callable[[_T], _T] = _default_validator
 ) -> _T:
-    """Decorates a function or the init-method of an objects with a pydantic
+    """Decorates a function or the init-method of an object with a pydantic
     validation decorator.
 
     This leverages `pydantic.validate_arguments`, which is currently in Beta [1]_.
@@ -36,23 +36,27 @@ def validates_with_pydantic(
 
     Notes
     -----
+    ``pydantic`` must be installed [2]_ as a separate dependency to leverage this validator.
+
     Users should be aware of pydantic's data conversion strategy [2]_; pydantic
     may cast data so that it will conform to its annotated type.
 
     References
     ----------
     .. [1] https://pydantic-docs.helpmanual.io/usage/validation_decorator/
-    .. [2] https://pydantic-docs.helpmanual.io/usage/models/#data-conversion
+    .. [2] https://pydantic-docs.helpmanual.io/install/
+    .. [3] https://pydantic-docs.helpmanual.io/usage/models/#data-conversion
 
     Examples
     --------
+    >>> from hydra_zen.experimental.third_party.pydantic import validates_with_pydantic
     >>> from pydantic import PositiveInt
     >>> def f(x: PositiveInt): return x
-    >>> val_f = validates_with_pydantic(needs_pos_int)
+    >>> val_f = validates_with_pydantic(f)
     >>> f(-100)
     -100
     >>> val_f(-100)
-    ValidationError: 1 validation error for F [...]
+    ValidationError: 1 validation error for F (...)
 
     >>> class A:
     ...     def __init__(self, x: PositiveInt): ...
@@ -60,6 +64,23 @@ def validates_with_pydantic(
     __main__.A
     >>> A(-10)
     ValidationError: 1 validation error for Init
+
+    This is designed to be used with the ``zen_wrappers`` feature of `builds`.
+
+    >>> from hydra_zen import builds, instantiate
+    >>> # instantiations of `conf` will be validated by pydantic
+    >>> conf = builds(f, populate_full_signature=True, zen_wrappers=validates_with_pydantic)
+    >>> instantiate(conf, x=10)
+    10
+    >>> instantiate(conf, x=-2)
+    ValidationError: 1 validation error for F (...)
+
+    Note that pydantic's data-coercion is ensures smooth compatibility with Hydra.
+    I.e. lists will be coerced to the appropriate annotated sequence type.
+
+    >>> def g(x: tuple): return x  # note the annotation
+    >>> validates_with_pydantic(g)([1, 2, 3])  # input: list, output: tuple
+    (1, 2, 3)
     """
     if inspect.isclass(obj) and hasattr(type, "__init__"):
         if hasattr(obj.__init__, "validate"):
