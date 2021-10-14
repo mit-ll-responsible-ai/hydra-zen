11796f3537d3a1a3f8cabbaf8155e165576ce6cc
jgbos <jgoodwin@ll.mit.edu>
2021-10-14 10:28:51 -0400

    Update src/hydra_zen/experimental.py
    Co-authored-by: Ryan Soklaski <ryan.soklaski@gmail.com>

--- src/hydra_zen/experimental.py
+++ src/hydra_zen/experimental.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2021 Massachusetts Institute of Technology
+# SPDX-License-Identifier: MIT
+
 import warnings
 from pathlib import Path
 from typing import Any, Callable, List, Mapping, Optional, Union
