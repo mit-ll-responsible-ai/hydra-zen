6743a645cd793cc1db77799fa03baf1b8e548d87
rsokl <rsokl@users.noreply.github.com>
2021-11-02 17:32:14 +0000

    deploy: 871d8a28f03e1c4b949ba33c9c8ce9a6e3037988

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0+8.g1a4502b',
+    VERSION: '0.3.0+9.g871d8a2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
