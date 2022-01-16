9f537eaa66c3740b30a05cf4bc298cb751cfcc2b
rsokl <rsokl@users.noreply.github.com>
2022-01-16 18:23:33 +0000

    deploy: cbca2c743f3d8031809d08faf11a0d2bd269f576

--- docs/_static/gtag.js
+++ docs/_static/gtag.js
@@ -1,9 +1,6 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115029372-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'UA-115029372-2');
-</script>
+gtag('config', 'UA-115029372-2');
+
