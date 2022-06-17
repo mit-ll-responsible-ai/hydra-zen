22c17c96d578c287b7746ded6f5a00ecbc39a9c3
Ryan Soklaski <ry26099@mit.edu>
2022-06-17 15:32:49 -0400

    update docs

--- src/hydra_zen/structured_configs/_implementations.py
+++ src/hydra_zen/structured_configs/_implementations.py
@@ -769,7 +769,7 @@ def builds(
 
         If ``None``, the ``_convert_`` attribute is not set on the resulting config.
 
-    hydra_defaults : List['_self_' | Dict[str, str]] | None, optional (default = None)
+    hydra_defaults : None | list[str | dict[str, str | list[str] | None ]], optional (default = None)
         A list in an input config that instructs Hydra how to build the output config
         [7]_ [8]_. Each input config can have a Defaults List as a top level element. The
         Defaults List itself is not a part of output config.
