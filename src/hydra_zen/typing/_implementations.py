09ffc97b8da723e46fd85d0cb12d942f57c32f2c
Ryan Soklaski <ryan.soklaski@ll.mit.edu>
2022-12-08 18:11:10 -0500

    isort should FAIL

--- src/hydra_zen/typing/_implementations.py
+++ src/hydra_zen/typing/_implementations.py
@@ -24,8 +24,8 @@ from typing import (
     Set,
     Tuple,
     Type,
-    TypeVar,
     Union,
+    TypeVar,
 )
 
 from omegaconf import DictConfig, ListConfig
@@ -68,7 +68,7 @@ T3 = TypeVar("T3")
 T4 = TypeVar("T4", bound=Callable[..., Any])
 
 
-InstOrType: TypeAlias = Union[T,    Type[T]]
+InstOrType: TypeAlias = Union[T, Type[T]]
 
 
 if TYPE_CHECKING:
