b01107cb477b0f7d9b25cc1f87ce964ff086484a
Ryan Soklaski <rsoklaski@gmail.com>
2023-11-24 11:15:42 -0500

    strengthen tests

--- tests/test_manual_target.py
+++ tests/test_manual_target.py
@@ -25,6 +25,10 @@ def foo(x=1, y=2):
     raise AssertionError("I should not get called")
 
 
+def passthrough(x):
+    return x
+
+
 @pytest.mark.parametrize(
     "target",
     [
@@ -33,11 +37,25 @@ def foo(x=1, y=2):
         builds(foo, populate_full_signature=True),
     ],
 )
-def test_manual(target):
-    assert instantiate(
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        {},
+        {"x": 1},
+        # overrides default with different value
+        pytest.param({"x": 2}, marks=pytest.mark.xfail),
+        # exercise zen_processing branch
+        {"zen_wrappers": passthrough},
+    ],
+)
+def test_manual(target, kwargs):
+    out = instantiate(
         builds(
             target,
             populate_full_signature=True,
             zen_dataclass={"target": "builtins.dict"},
+            **kwargs,
         )
-    ) == dict(x=1, y=2)
+    )
+    assert isinstance(out, dict)
+    assert out == dict(x=1, y=2)
