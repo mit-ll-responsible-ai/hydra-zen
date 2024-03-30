055eb9beb4e6f517f159357d879be525f14f673a
Ryan Soklaski <rsoklaski@gmail.com>
2024-03-30 11:32:48 -0400

    pydantic parsing prototype!

--- src/hydra_zen/third_party/pydantic.py
+++ src/hydra_zen/third_party/pydantic.py
@@ -132,3 +132,108 @@ def validates_with_pydantic(
         obj = cast(_T, validator(obj))
 
     return cast(_T, obj)
+
+
+def prototype():
+
+    from functools import wraps
+    from typing import Any, Callable, Dict, Literal, Tuple
+
+    import pydantic as pyd
+
+    from hydra_zen import builds, instantiate as old_instantiate
+
+    # note: this is super weird and busted when the callable is
+    # `int` or `dict`
+    # TODO: fix for builtins
+    # TODO: handle __init__/__new__ gracefully: parse(Foo.__init__)(Foo, 'aa')
+    #       -- maybe clone signature??
+    parse = pyd.validate_call(config={"arbitrary_types_allowed": True})
+
+    def _call_target(
+        _target_: Callable[..., Any],
+        _partial_: bool,
+        args: Tuple[Any, ...],
+        kwargs: Dict[str, Any],
+        full_key: str,
+    ) -> Any:
+        """Call target (type) with args and kwargs."""
+        import functools
+
+        from hydra._internal.instantiate._instantiate2 import (
+            _convert_target_to_string,
+            _extract_pos_args,
+        )
+        from hydra.errors import InstantiationException
+        from omegaconf import OmegaConf
+
+        try:
+            args, kwargs = _extract_pos_args(args, kwargs)
+            # detaching configs from parent.
+            # At this time, everything is resolved and the parent link can cause
+            # issues when serializing objects in some scenarios.
+            for arg in args:
+                if OmegaConf.is_config(arg):
+                    arg._set_parent(None)
+            for v in kwargs.values():
+                if OmegaConf.is_config(v):
+                    v._set_parent(None)
+        except Exception as e:
+            msg = (
+                f"Error in collecting args and kwargs for '{_convert_target_to_string(_target_)}':"
+                + f"\n{repr(e)}"
+            )
+            if full_key:
+                msg += f"\nfull_key: {full_key}"
+
+            raise InstantiationException(msg) from e
+
+        if _partial_:
+            try:
+                return functools.partial(parse(_target_), *args, **kwargs)
+            except Exception as e:
+                msg = (
+                    f"Error in creating partial({_convert_target_to_string(_target_)}, ...) object:"
+                    + f"\n{repr(e)}"
+                )
+                if full_key:
+                    msg += f"\nfull_key: {full_key}"
+                raise InstantiationException(msg) from e
+        else:
+            try:
+                print(args, kwargs)
+                return parse(_target_)(*args, **kwargs)
+            except Exception as e:
+                msg = f"Error in call to target '{_convert_target_to_string(_target_)}':\n{repr(e)}"
+                if full_key:
+                    msg += f"\nfull_key: {full_key}"
+                raise InstantiationException(msg) from e
+
+    def loudly(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            print(f"Calling {fn.__name__}")
+            return fn(*args, **kwargs)
+
+        return wrapper
+
+    def instantiate(x):
+        try:
+            from hydra._internal.instantiate import _instantiate2 as inst
+
+            old = inst._call_target
+            inst._call_target = loudly(_call_target)
+            return old_instantiate(x)
+        finally:
+            inst._call_target = old
+
+    def ff(x: tuple[int, int]):
+        return x
+
+    from dataclasses import dataclass
+
+    @dataclass
+    class A:
+        x: "Literal[1] | A"
+
+    instantiate(builds(A, x=builds(A, x=2)))
