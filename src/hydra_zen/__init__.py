d04dae071ec304e24d866db190f9e17350a3871a
Ryan Soklaski <rsoklaski@gmail.com>
2025-01-18 16:35:15 -0500

    monkey patch out quadratic config store copy

--- src/hydra_zen/__init__.py
+++ src/hydra_zen/__init__.py
@@ -1,5 +1,39 @@
 # Copyright (c) 2025 Massachusetts Institute of Technology
 # SPDX-License-Identifier: MIT
+
+
+# noqa: E402
+def _monkeypatch_hydra():
+    from hydra._internal.config_loader_impl import ConfigLoaderImpl, OverridesParser
+    from hydra._internal.config_repository import (
+        CachingConfigRepository,
+        ConfigRepository,
+    )
+
+    def _new_init(self, delegate):
+        self.delegate = delegate
+        self.cache = {}
+
+    CachingConfigRepository.__init__ = _new_init
+
+    def _parse_overrides_and_create_caching_repo(self, config_name, overrides):
+        parser = OverridesParser.create()
+        parsed_overrides = parser.parse_overrides(overrides=overrides)
+        caching_repo = CachingConfigRepository(
+            ConfigRepository(config_search_path=self.config_search_path)
+        )
+        self._process_config_searchpath(config_name, parsed_overrides, caching_repo)
+        return parsed_overrides, caching_repo
+
+    ConfigLoaderImpl._parse_overrides_and_create_caching_repo = (
+        _parse_overrides_and_create_caching_repo
+    )
+    print("teehee")
+
+
+_monkeypatch_hydra()
+del _monkeypatch_hydra
+
 from typing import TYPE_CHECKING
 
 from ._hydra_overloads import (
